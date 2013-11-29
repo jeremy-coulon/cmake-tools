4e49edc467a2b4192abdf648c04b306ced8ab0d2
Jeremy Coulon <jeremy.coulon@free.fr>
2013-11-30 00:49:08 +0100

    Linker flags for setting DT_RUNPATH

--- DefaultBuildFlags.cmake
+++ DefaultBuildFlags.cmake
@@ -6,7 +6,7 @@
 #   http://www.boost.org/LICENSE_1_0.txt
 #=============================================================================
 
-if(CMAKE_COMPILER_IS_GNUC)
+if(CMAKE_COMPILER_IS_GNUCC)
     set(CMAKE_C_FLAGS_INIT "-Wall")
     set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "-O3 -DNDEBUG -g")
 endif()
@@ -14,6 +14,14 @@ endif()
 if(CMAKE_COMPILER_IS_GNUCXX)
     set(CMAKE_CXX_FLAGS_INIT "-Wall")
     set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-O3 -DNDEBUG -g")
+endif()
+
+if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+    # Set DT_RUNPATH instead of DT_RPATH
+    # It allows the user to override rpath at runtime
+    # by environment variable LD_LIBRARY_PATH
+    set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--enable-new-dtags")
+    set(CMAKE_SHARED_LINKER_FLAGS_INIT "-Wl,--enable-new-dtags")
 endif()
 
 if((NOT DEFINED CMAKE_BUILD_TYPE) OR (CMAKE_BUILD_TYPE STREQUAL ""))
